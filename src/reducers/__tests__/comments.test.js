d1f32534c7ea88c7acb8e081c22a2ead3314f26b
Anson Felder <afelder09@gmail.com>
2018-10-08 16:38:57 -0400

    Adding action creator tests

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -7,4 +7,9 @@ it('handles actions of type SAVED_COMMENT', () => {
     const newState = commentsReducer([], action);
 
     expect(newState).toEqual(['New comment']);
+});
+
+it('handles action with unknown type', () => {
+    const newState = commentsReducer([], {});
+    expect(newState).toEqual([]);
 })
