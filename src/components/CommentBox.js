e39a9e235ba5bee2647119ab56bf0ba7c4fccbb4
Anson Felder <afelder09@gmail.com>
2018-10-15 08:33:37 -0400

    Finished testing project

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -20,13 +20,17 @@ class CommentBox extends Component {
         
     render() {
         return(
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a comment</h4>
-                <textarea value={this.state.comment} onChange={this.handleChange}/>
-                <div>
-                    <button>Submit Button</button>
-                </div>
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h4>Add a comment</h4>
+                    <textarea value={this.state.comment} onChange={this.handleChange}/>
+                    <div>
+                        <button>Submit Button</button>
+                    </div>
+                </form>
+                <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
+            
         )
     }
 }
